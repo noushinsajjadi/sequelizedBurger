a7b61425171e1722e4945dcfa1878a185445a51b
Nooshin sajadi <nooshinsajadi@Nooshins-MacBook-Pro.local>
2017-05-10 18:23:06 -0400

    First Commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name,date) VALUES ('chicken bacon burger',NOW());
+INSERT INTO burgers (burger_name,date) VALUES ('beaf double cheese burger',NOW());
+INSERT INTO burgers (burger_name, devoured , date) VALUES ('Happy meal burger', true ,NOW());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('beaf spicy burger', true ,NOW());
+INSERT INTO burgers (burger_name, devoured , date) VALUES ('chicken spicy burger', true ,NOW());
